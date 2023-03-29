c14ff11345d87df955dcee4d195a63aa02c5828a
QuentinVigier <quentin.vigier12@gmail.com>
2023-03-29 16:54:04 +0200

    Update 1

---
+++ Bear Note/notes.js
@@ -0,0 +1,24 @@
+//Récupérer les notes déja existantes sur le pc
+function getNotes () {
+}
+
+//Sauvegarder les notes
+function saveNotes () {
+}
+
+//Créer une nouvelle note à partir du bouton
+function createNoteElement(){
+}
+
+//Remplir nouvelle note
+function addNote(){
+}
+
+// Sauvergarder le prise de note
+function updateNote(){
+
+}
+
+function deleteNote(){
+    
+}
